6d5679a6b36f5589ec242c07d46337c363ff31f8
jatin177128 <aryajatin177@gmail.com>
2024-10-05 00:39:00 +0530

    ui change

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -14,7 +14,7 @@ import { Mail } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { useSession } from "next-auth/react"
-
+import { motion } from 'framer-motion';
 
 const Home = () => {
   const router = useRouter();
@@ -28,11 +28,32 @@ const Home = () => {
     }
   }
   return (
-      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
-        <section className="text-center mb-8 md:mb-12">
-          <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the World of Anonymous Feedback
-          </h1>
+    <>
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-white text-white">
+        <section className="text-center mb-8 md:mb-12 ">
+        <div className='textstructure'>
+            {["Dive into the","World of ","Real Feedback"].map((item,index)=>{
+            return (
+                <div className='masker '>
+                <div className='w-fit flex mr-[1vw] '>
+                    {index === 1 && (
+                      <motion.div
+                        initial={{ width: 0 }}
+                        animate={{ width: "13vw" }}
+                        transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
+                        className="mr-[1vw] w-[8vw] rounded-md h-[5vw] top-[0.65vw] relative"
+                      >
+                        <img className="ml-[1vw] pb-[1vw] h-[5vw] w-[8vw] overflow-hidden" src="https://cdn.prod.website-files.com/6082ee0e95eb6459d78fac06/6657aad705a9dfa3988a4270_6349c1990124c0b3efac2dcd_Types-of-hackers-header-image.png" alt="ochii" />
+                      </motion.div>
+                    )}
+                    <h1 className=" text-black uppercase text-[5vw] md:text-[3vw] lg:text-[6vw] leading-[5vw]  tracking-tighter font-mono font-semibold">
+                      {item}
+                    </h1>
+                </div>
+                </div>
+            );
+        })}
+        </div>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
           Anonymous Feedback - Where your identity remains a secret.
           </p>
@@ -64,8 +85,11 @@ const Home = () => {
             ))}
           </CarouselContent>
         </Carousel>
-        <Button className="bg-blue-500" onClick={handleSeeMessages}>See Messages</Button>
+        <Button  onClick={handleSeeMessages}>See Messages</Button>
       </main>
+      </>
+
+  
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,11 @@ import { useSession , signOut } from 'next-auth/react'
 import { User} from 'next-auth';
 import { Button } from './ui/button';
 import { useRouter } from 'next/navigation';
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+  } from "@/components/ui/avatar"
 
 const Navbar = () => {
     const router = useRouter();
@@ -15,14 +20,17 @@ const Navbar = () => {
     }
 
   return (
-    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
-        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+    <nav data-theme = 'coffee' className="p-4 md:p-6 shadow-md  text-white font-serif font-semibold">
+        <div className="container mx-auto flex flex-col md:flex-row gap-3 items-center">
+                  <Avatar>
+                     <AvatarImage src="https://svg.io/_next/image?url=https%3A%2F%2Fprod-svgio-infrastructure-contentbucketdd05a3b9-853uj7uztypy.s3.amazonaws.com%2F70%2F9f%2Fcd%2F0e%2Fa-logo-for-anonymous-feedback.png&w=1080&q=75" alt="@shadcn" />
+                     </Avatar>
             <a href="#" onClick={handleTrueFeedbackHome} className="text-xl font-bold mb-4 md:mb-0">Anonymous Feedback</a>
             {
                 session ? (
                     <>
-                    <span className="mr-4">Welcome , {user?.username||user?.email}</span>
-                    <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>Logout</Button>
+                    <span className="mr-[30vw] ml-[25vw] items-center">Welcome , {user?.username||user?.email}</span>
+                    <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black  " variant='outline'>Logout</Button>
                     </>
                 ):(
                     <Link href={'/sign-in'}>
@@ -36,3 +44,5 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+

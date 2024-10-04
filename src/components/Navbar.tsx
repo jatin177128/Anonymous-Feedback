9dcd7cf4d77bac16de5279de576de57e43cd58d7
jatin177128 <aryajatin177@gmail.com>
2024-10-05 01:43:22 +0530

    nav-messu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             {
                 session ? (
                     <>
-                    <span className="mr-[30vw] ml-[25vw] items-center">Welcome , {user?.username||user?.email}</span>
+                    <span className="mr-[30vw] ml-[22vw] items-center">Welcome , {user?.username||user?.email}</span>
                     <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black  " variant='outline'>Logout</Button>
                     </>
                 ):(
